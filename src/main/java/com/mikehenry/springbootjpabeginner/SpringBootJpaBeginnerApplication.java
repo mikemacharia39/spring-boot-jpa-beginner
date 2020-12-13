a39fe60eea9f4fd3db098272da780bad0680100d
mikemacharia39 <mikemacharia39@gmail.com>
2020-12-13 20:26:42 +0300

    Addition of application shell

---
+++ src/main/java/com/mikehenry/springbootjpabeginner/SpringBootJpaBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.mikehenry.springbootjpabeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaBeginnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikehenry/springbootjpabeginner/SpringBootJpaBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikehenry.springbootjpabeginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
